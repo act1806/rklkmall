cf500b2b1a0ed3ab1ce537cbe4969fa32e8d770e
act1806 <spidermanan_an@sina.com>
2019-12-21 19:23:29 +0800

    1
    222

--- litemall-admin/config/index.js
+++ litemall-admin/config/index.js
@@ -8,7 +8,7 @@ module.exports = {
   dev: {
     // Paths
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/',
+    assetsPublicPath: '/rklkmall-admin',
     proxyTable: {},
 
     // Various Dev Server settings
